224cb269b61ecd8f7a3b2d583f5ce30d5518a476
Christopher Kim <christopherkim@berkeley.edu>
2026-04-08 19:25:44 -0700

    DeploymentVLRagain

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,5 +1,5 @@
 const API_BASE =
-  (process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000').replace(/\/$/, '');
+  (process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:5000').replace(/\/$/, '');
 
 async function handleResponse<T>(response: Response): Promise<T> {
   if (!response.ok) {
